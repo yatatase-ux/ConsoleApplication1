afb56228543ca198a85f8b8bf6f793268a2a6863
山下大星 <yatatase@gmail.com>
2026-05-12 15:10:57 +0900

    whileを使って同じ処理をする関数を実装

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,14 +1,24 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 
-void Cout_HW_100()
+void Cout_HW_100_for()
 {
 	for (int count = 0; count < 100; count++) {
 		std::cout << count + 1 << " : Hello World!\n";
 	}
 }
 
+void Cout_HW_100_while()
+{
+	int count = 100;
+	while (count--)
+	{
+		std::cout << 100 - count << " : Hello World!\n";
+	}
+}
+
 int main()
 {
-	Cout_HW_100();
+//	Cout_HW_100_for();
+
+	Cout_HW_100_while();
 }
