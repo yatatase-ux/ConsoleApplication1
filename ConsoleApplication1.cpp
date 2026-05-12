32e58ec1ad3cd9b4627b8f167dca742d9e2ef312
山下大星 <yatatase@gmail.com>
2026-05-12 15:36:34 +0900

    2回に1回、文字を反転させる関数を追加

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <string>
 
 void Cout_HW_100_for()
 {
@@ -16,9 +17,42 @@ void Cout_HW_100_while()
 	}
 }
 
+void Inversion_HW()
+{
+	const char hw[13] = "Hello World!";
+
+	for (int count = 0; count < 20; count++)
+	{
+		if (count % 2 == 0)
+		{
+		//	int hw_size = sizeof(hw);
+
+			std::cout << count + 1 << " : ";
+			for (int char_num = 0; char_num < 13; char_num++)
+			{
+				std::cout << hw[char_num];
+			}
+			std::cout << std::endl;
+		}
+		else
+		{
+		//	int hw_size = sizeof(hw);
+
+			std::cout << count + 1 << " : ";
+			for (int char_num = 12; char_num >= 0; char_num--)
+			{
+				std::cout << hw[char_num];
+			}
+			std::cout << std::endl;
+		}
+	}
+}
+
 int main()
 {
 //	Cout_HW_100_for();
 
-	Cout_HW_100_while();
+//	Cout_HW_100_while();
+
+	Inversion_HW();
 }
